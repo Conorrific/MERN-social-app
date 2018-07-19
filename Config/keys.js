caa7f129e4400159793ecfafdd9234fffd8df145
conorrific <conorbellchambers@gmail.com>
2018-07-19 08:22:25 -0500

    Updated login/registration and added token

--- Config/keys.js
+++ Config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://cbell255:Hidnar11@ds239681.mlab.com:39681/cmern"
+  mongoURI: "mongodb://cbell255:Hidnar11@ds239681.mlab.com:39681/cmern",
+  secretOrKey: "secret"
 };
