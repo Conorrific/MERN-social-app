a65957332ea791e3f0d4823e728bdceb34ae6f36
conorrific <conorbellchambers@gmail.com>
2018-07-24 15:34:46 -0500

    fixed bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,8 +5,8 @@ const passport = require("passport");
 
 // Load Validation
 const validateProfileInput = require("../../validation/profile");
-// const validateExperienceInput = require('../../validation/experience');
-// const validateEducationInput = require('../../validation/education');
+const validateExperienceInput = require("../../validation/experience");
+// const validateEducationInput = require("../../validation/education");
 
 // Load Profile Model
 const Profile = require("../../models/Profile");
@@ -173,6 +173,14 @@ router.post(
   "/experience",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     Profile.findOne({ user: req.user.id }).then(profile => {
       const newExp = {
         title: req.body.title,
@@ -184,7 +192,7 @@ router.post(
         description: req.body.description
       };
 
-      // Add experience to array
+      // Add to exp array
       profile.experience.unshift(newExp);
 
       profile.save().then(profile => res.json(profile));
