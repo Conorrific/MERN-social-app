9a6f34ddee382583fadc16689b88595f2ecabd14
conorrific <conorbellchambers@gmail.com>
2018-08-09 06:14:05 -0500

    post and remove comments

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS, GET_POST, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+  DELETE_POST
+} from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -20,14 +27,14 @@ export const addPost = postData => dispatch => {
     );
 };
 
-// Get Post
-export const getPost = () => dispatch => {
-  dispatch(SetPostLoading());
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
   axios
     .get("/api/posts")
     .then(res =>
       dispatch({
-        type: GET_POST,
+        type: GET_POSTS,
         payload: res.data
       })
     )
@@ -39,8 +46,72 @@ export const getPost = () => dispatch => {
     );
 };
 
+// Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
+        payload: null
+      })
+    );
+};
+
+// Delete Post
+
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
-export const SetPostLoading = () => {
+export const setPostLoading = () => {
   return {
     type: POST_LOADING
   };
